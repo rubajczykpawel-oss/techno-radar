5bb9a27dd550413ed5c425e5be19ff75e08b4f8b
Paweł Rubajczyk <rubajczykpawel@gmail.com>
2026-05-26 19:36:23 +0200

    Add Polish city display names

--- services/event_response_service.py
+++ services/event_response_service.py
@@ -1,10 +1,12 @@
 from services.date_service import get_polish_day_of_week, get_polish_formatted_date
+from services.city_service import get_polish_city_name
 
 def build_event_response(event):
     return {
         "id": event.id,
         "name": event.name,
         "city": event.city,
+        "city_display": get_polish_city_name(event.city),
         "date": event.date,
         "day_of_week": get_polish_day_of_week(event.date),
         "formatted_date": get_polish_formatted_date(event.date),
